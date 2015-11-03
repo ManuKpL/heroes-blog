4efa7987be2c6b79cf137389e3b9c068ca8876b9
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-11-03 03:01:11 +0100

    custom user edit form

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,18 @@
 ActiveAdmin.register User do
+  form do |f|
+    f.inputs "Identity" do
+      f.input :name
+      f.input :email
+      f.input :twitter_name
+    end
+    f.inputs "Admin" do
+      f.input :admin
+    end
+    f.actions
+  end
+
+  permit_params :name, :email, :admin
+
   index do
     selectable_column
     column :id
