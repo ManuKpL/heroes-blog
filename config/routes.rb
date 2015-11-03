88f1e62f4f0f6fa81d8e095e6f8c7cc132907bb0
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-11-03 01:08:33 +0100

    initialized the new project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   devise_for :users
   root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
